7e2c0f14e58799725af4d400c1ad56d1749cb4eb
heping <ping.he@chelaile.net.cn>
2019-07-03 11:44:54 +0800

    new photo assets project

---
+++ PHAssetsTest/PHAssetsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PHAssetsTest
+//
+//  Created by HePing on 2019/7/3.
+//  Copyright © 2019 HePing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
