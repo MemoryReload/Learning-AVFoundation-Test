1cc99ca32aaa6ae9510917c089f71f25c425c5b8
heping <ping.he@chelaile.net.cn>
2019-07-03 14:45:13 +0800

    collections

--- PHAssetsTest/PHAssetsTest/ViewController.m
+++ PHAssetsTest/PHAssetsTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Photos/Photos.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self authorizeWithCompletionHandler:^(BOOL authorized) {
+        if (authorized) {
+            //fucking loading data now
+            [self loadCollections];
+        }
+        else{
+            //give the fucking alert
+            UIAlertController* alertVC = [UIAlertController alertControllerWithTitle:@"提示" message:@"请xxxx打开相册使用权限" preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction* action = [UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleDefault handler:nil];
+            [alertVC addAction:action];
+            [self.navigationController pushViewController:alertVC animated:YES];
+        }
+    }];
 }
 
+#pragma mark -  authorize method
+- (void)authorizeWithCompletionHandler:(void(^)(BOOL authorized))completionBlock
+{
+   PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
+    switch (status) {
+        case PHAuthorizationStatusRestricted:
+        case PHAuthorizationStatusDenied:
+            !completionBlock?:completionBlock(NO);
+            break;
+        case PHAuthorizationStatusAuthorized:
+            !completionBlock?:completionBlock(YES);
+            break;
+        case PHAuthorizationStatusNotDetermined:
+        {
+            [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+                [self authorizeWithCompletionHandler:completionBlock];
+            }];
+        }
+            break;
+        default:
+            break;
+    }
+}
 
+-(void)loadCollections
+{
+    
+}
 @end
